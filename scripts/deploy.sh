20f75fb3267a72fd4095252d15e67f666d1885f4
Ben <ben39053372@gmail.com>
2020-03-31 11:21:34 +0800

    chagne port

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@ git pull
 docker stop insurance-cms-nginx
 docker rm insurance-cms-nginx
 docker build -t insurance-cms-nginx .
-docker run -p 8081:80 -p 8443:443 -d --name insurance-cms-nginx insurance-cms-nginx
+docker run -p 8081:80 -d --name insurance-cms-nginx insurance-cms-nginx
